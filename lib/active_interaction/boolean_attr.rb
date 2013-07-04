ff6b21725a9c82ae8c69164731ec6c92587cccce
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-07-03 19:21:42 -0500

    remove unnecessary default options

--- lib/active_interaction/boolean_attr.rb
+++ lib/active_interaction/boolean_attr.rb
@@ -2,13 +2,7 @@ module ActiveInteraction
   module BooleanAttr
     extend Attr
 
-    DEFAULT_OPTIONS = {
-      allow_nil: false
-    }.freeze
-
     def self.prepare(value, options = {})
-      options = DEFAULT_OPTIONS.merge(options)
-
       case value
         when TrueClass, FalseClass
           value
--- lib/active_interaction/integer_attr.rb
+++ lib/active_interaction/integer_attr.rb
@@ -2,13 +2,7 @@ module ActiveInteraction
   module IntegerAttr
     extend Attr
 
-    DEFAULT_OPTIONS = {
-      allow_nil: false
-    }.freeze
-
     def self.prepare(value, options = {})
-      options = DEFAULT_OPTIONS.merge(options)
-
       case value
         when Integer
           value
