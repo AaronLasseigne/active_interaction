0e48aec027537f7208eb672895e6e6f1badb71c4
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-07-10 17:17:45 -0500

    fix #14 by passing the block so that it does not get sucked into *args in method_missing

--- lib/active_interaction/base.rb
+++ lib/active_interaction/base.rb
@@ -159,7 +159,7 @@ module ActiveInteraction
       if args.length == 0 && !block_given?
         super
       elsif block_given?
-        method_missing(:hash, *args, block)
+        method_missing(:hash, *args, &block)
       else
         method_missing(:hash, *args)
       end
