56815fb1a331cc445194fb1b2d7965ba7dab65c9
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-07-11 11:18:39 -0500

    slight refactor in array filter

--- lib/active_interaction/filters/array_filter.rb
+++ lib/active_interaction/filters/array_filter.rb
@@ -13,17 +13,21 @@ module ActiveInteraction
     def self.convert_values(values, &block)
       return values.dup unless block_given?
 
-      filter_methods = FilterMethods.evaluate(&block)
-      if filter_methods.count > 1
-        raise ArgumentError, 'Array filter blocks can only contain one filter.'
-      else
-        filter_method = filter_methods.first
-      end
+      filter_method = get_filter_method(FilterMethods.evaluate(&block))
 
       values.map do |value|
         Filter.factory(filter_method.method_name).prepare(filter_method.attribute, value, filter_method.options, &filter_method.block)
       end
     end
     private_class_method :convert_values
+
+    def self.get_filter_method(filter_methods)
+      if filter_methods.count > 1
+        raise ArgumentError, 'Array filter blocks can only contain one filter.'
+      else
+        filter_method = filter_methods.first
+      end
+    end
+    private_class_method :get_filter_method
   end
 end
