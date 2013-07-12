23a57e5f32873d190d9bad8eb48a314af6679d31
Taylor Fausak <taylor@fausak.me>
2013-07-12 14:57:41 -0500

    Give specs better descriptions

--- spec/active_interaction/integration/default_interaction_spec.rb
+++ spec/active_interaction/integration/default_interaction_spec.rb
@@ -15,54 +15,66 @@ end
 describe DefaultInteraction do
   include_context 'interactions'
 
-  it 'uses the default' do
-    expect(result[0]).to eq []
+  context 'with a default' do
+    it 'uses the default' do
+      expect(result[0]).to eq []
+    end
+
+    it 'uses the option instead of default' do
+      array = [rand]
+      options.merge!(array: array)
+      expect(result[0]).to eq array
+    end
   end
 
-  it 'uses the option instead of default' do
-    array = [rand]
-    options.merge!(array: array)
-    expect(result[0]).to eq array
+  context 'with a nil default' do
+    it 'uses the default' do
+      expect(result[1]).to be_nil
+    end
   end
 
-  it 'uses nil as the default' do
-    expect(result[1]).to be_nil
+  context 'with a default with a constraint' do
+    it 'uses the default' do
+      expect(result[2]).to eq [true]
+    end
   end
 
-  it 'uses the default' do
-    expect(result[2]).to eq [true]
-  end
-
-  it do
-    expect {
-      class InteractionWithInvalidDefaultArray < ActiveInteraction::Base
-        array :a, default: Object.new
-        def execute; end
-      end
-    }.to raise_error ActiveInteraction::InvalidDefaultValue
-  end
-
-  it do
-    expect {
-      class InteractionWithInvalidNestedDefaultArray < ActiveInteraction::Base
-        array :a, default: [Object.new] do
-          array
+  context 'with an invalid default' do
+    it 'raises an error' do
+      expect {
+        class InteractionWithInvalidDefaultArray < ActiveInteraction::Base
+          array :a, default: Object.new
+          def execute; end
         end
-        def execute; end
-      end
-    }.to raise_error ActiveInteraction::InvalidDefaultValue
+      }.to raise_error ActiveInteraction::InvalidDefaultValue
+    end
   end
 
-  it do
-    expect {
-      class InteractionWithInvalidlyNestedDefault < ActiveInteraction::Base
-        array :a do
-          array default: []
+  context 'with an invalid default with a constraint' do
+    it 'raises an error' do
+      expect {
+        class InteractionWithInvalidNestedDefaultArray < ActiveInteraction::Base
+          array :a, default: [Object.new] do
+            array
+          end
+          def execute; end
         end
-        def execute; end
-      end
-      # TODO: We should fail when defining the class, not when trying to run it.
-      InteractionWithInvalidlyNestedDefault.run(a: [])
-    }.to raise_error ArgumentError
+      }.to raise_error ActiveInteraction::InvalidDefaultValue
+    end
+  end
+
+  context 'with an invalidly nested default' do
+    it do
+      expect {
+        class InteractionWithInvalidlyNestedDefault < ActiveInteraction::Base
+          array :a do
+            array default: []
+          end
+          def execute; end
+        end
+        # TODO: We should fail when defining the class, not when trying to run it.
+        InteractionWithInvalidlyNestedDefault.run(a: [])
+      }.to raise_error ArgumentError
+    end
   end
 end
