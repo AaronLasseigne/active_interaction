f6b1dd72a26c37a27ff68110c716fdee80b1d165
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-09-23 09:37:20 -0500

    updating hash attribute documentation to reflect new default syntax

--- lib/active_interaction/filters/hash_filter.rb
+++ lib/active_interaction/filters/hash_filter.rb
@@ -3,7 +3,11 @@ module ActiveInteraction
     # Creates accessors for the attributes and ensures that values passed to
     #   the attributes are Hashes.
     #
-    # @macro attribute_method_params
+    # @param *attributes [Symbol] One or more attributes to create.
+    # @param options [Hash]
+    # @option options [Boolean] :allow_nil Allow a `nil` value.
+    # @option options [Object] :default Set to `{}` to allow defaults. Defaults
+    #   on keys will bubble up to populate the empty hash.
     # @param block [Proc] Filter methods to apply for select keys.
     #
     # @example
@@ -15,6 +19,20 @@ module ActiveInteraction
     #     model :item
     #     integer :quantity
     #     boolean :delivered
+    #   end
+    #
+    # @example A Hash with default keys. If a hash is provided the key defaults are substituted for missing values.
+    #   hash :order do
+    #     model :account
+    #     model :item
+    #     integer :quantity, default: 1
+    #     boolean :delivered, default: false
+    #   end
+    #
+    # @example A Hash with a default. If nothing is passed the key defaults will be returned or an empty hash if there are no key defaults.
+    #   hash :order, default: {} do
+    #     integer :quantity, default: 1
+    #     boolean :delivered, default: false
     #   end
     #
     # @method self.hash(*attributes, options = {}, &block)
