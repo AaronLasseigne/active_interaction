3e7a6f03e3d66d61d7c592a7ed60f606c2e36efd
Taylor Fausak <taylor@fausak.me>
2013-10-16 16:52:29 -0500

    Fix #58; add basic implementation of symbolic errors

--- lib/active_interaction/errors.rb
+++ lib/active_interaction/errors.rb
@@ -4,4 +4,31 @@ module ActiveInteraction
   InvalidNestedValue = Class.new(StandardError)
   InvalidValue = Class.new(StandardError)
   MissingValue = Class.new(StandardError)
+
+  # TODO
+  # @since 0.5.1
+  class Errors < ActiveModel::Errors
+    # TODO
+    # @return [Hash{Symbol, Array<Symbol>}]
+    attr_reader :sym_messages
+
+    # TODO
+    # @param attribute [Symbol]
+    # @param symbol [Symbol]
+    # @param message [String, Symbol, Proc]
+    # @param options [Hash]
+    # @return [Hash{Symbol, Array<String>}]
+    def sym_add(attribute, symbol = :invalid, message = nil, options = {})
+      sym_messages[attribute] ||= []
+      sym_messages[attribute] << symbol
+
+      add(attribute, message || symbol, options)
+    end
+
+    # @private
+    def initialize(*args)
+      @sym_messages = {}
+      super
+    end
+  end
 end
---
+++ spec/active_interaction/errors_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe ActiveInteraction::Errors do
+  let(:klass) do
+    Class.new do
+      include ActiveModel::Model
+
+      attr_reader :attribute
+
+      def self.name
+        SecureRandom.hex
+      end
+    end
+  end
+
+  subject(:errors) { described_class.new(klass.new) }
+
+  describe '#sym_add' do
+    it do
+      errors.sym_add(:attribute)
+      expect(errors.sym_messages).to eq({ attribute: [:invalid] })
+    end
+
+    it do
+      errors.sym_add(:attribute, :symbol)
+      expect(errors.sym_messages).to eq({ attribute: [:symbol] })
+    end
+
+    it do
+      errors.sym_add(:attribute, :symbol, 'message')
+      expect(errors.sym_messages).to eq({ attribute: [:symbol] })
+    end
+
+    it do
+      errors.sym_add(:attribute, :symbol, 'message', { key: :value })
+      expect(errors.sym_messages).to eq({ attribute: [:symbol] })
+    end
+
+    context do
+      before do
+        allow(errors).to receive(:add)
+      end
+
+      it do
+        errors.sym_add(:attribute)
+        expect(errors).to have_received(:add).once.
+          with(:attribute, :invalid, {})
+      end
+
+      it do
+        errors.sym_add(:attribute, :symbol)
+        expect(errors).to have_received(:add).once.
+          with(:attribute, :symbol, {})
+      end
+
+      it do
+        errors.sym_add(:attribute, :symbol, 'message')
+        expect(errors).to have_received(:add).once.
+          with(:attribute, 'message', {})
+      end
+
+      it do
+        errors.sym_add(:attribute, :symbol, 'message', { key: :value })
+        expect(errors).to have_received(:add).once.
+          with(:attribute, 'message', { key: :value })
+      end
+    end
+  end
+end
