276b88b793a289907d09988419e8766250e6cd2e
Taylor Fausak <taylor@fausak.me>
2013-10-17 11:23:46 -0500

    Call #add before adding symbolic errors
    This means we can control the return type. Also, if `:strict` (in the
    options) is true, it will raise before getting to our code.

--- lib/active_interaction/errors.rb
+++ lib/active_interaction/errors.rb
@@ -15,21 +15,23 @@ module ActiveInteraction
 
     # Adds a symbolic error message to an attribute.
     #
-    # @example
+    # @example Adding a symbolic error.
     #   errors.add_sym(:attribute)
     #   errors.symbolic
     #   # => {:attribute=>[:invalid]}
+    #   errors.messages
+    #   # => {:attribute=>["is invalid"]}
     # @param attribute [Symbol] The attribute to add an error to.
     # @param symbol [Symbol] The symbolic error to add.
     # @param message [String, Symbol, Proc]
     # @param options [Hash]
-    # @return [Hash{Symbol => Array<String>}]
+    # @return [Hash{Symbol => Array<Symbol>}]
     # @see ActiveModel::Errors#add
     def add_sym(attribute, symbol = :invalid, message = nil, options = {})
+      add(attribute, message || symbol, options)
+
       symbolic[attribute] ||= []
       symbolic[attribute] << symbol
-
-      add(attribute, message || symbol, options)
     end
 
     # @private
