5ae2dc37bd3507612ab86b2df8f41d1e0b9a0966
Taylor Fausak <taylor@fausak.me>
2013-10-17 11:22:10 -0500

    Document ActiveInteraction::Errors

--- lib/active_interaction/errors.rb
+++ lib/active_interaction/errors.rb
@@ -5,18 +5,26 @@ module ActiveInteraction
   InvalidValue = Class.new(StandardError)
   MissingValue = Class.new(StandardError)
 
-  # TODO
+  # A small extension to provide symbolic error messages to make introspecting
+  #   and testing easier.
   class Errors < ActiveModel::Errors
-    # TODO
+    # A hash mapping attributes to arrays of symbolic messages.
+    #
     # @return [Hash{Symbol => Array<Symbol>}]
     attr_reader :symbolic
 
-    # TODO
-    # @param attribute [Symbol]
-    # @param symbol [Symbol]
+    # Adds a symbolic error message to an attribute.
+    #
+    # @example
+    #   errors.add_sym(:attribute)
+    #   errors.symbolic
+    #   # => {:attribute=>[:invalid]}
+    # @param attribute [Symbol] The attribute to add an error to.
+    # @param symbol [Symbol] The symbolic error to add.
     # @param message [String, Symbol, Proc]
     # @param options [Hash]
     # @return [Hash{Symbol => Array<String>}]
+    # @see ActiveModel::Errors#add
     def add_sym(attribute, symbol = :invalid, message = nil, options = {})
       symbolic[attribute] ||= []
       symbolic[attribute] << symbol
