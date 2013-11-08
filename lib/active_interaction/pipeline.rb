c3ddbd1ff51ea030e256b52df8d1522ab1f95b1c
Taylor Fausak <taylor@fausak.me>
2013-11-08 16:45:49 -0600

    Extract bind into helper method
    It's not exactly a monad, but it's pretty close.

--- lib/active_interaction/pipeline.rb
+++ lib/active_interaction/pipeline.rb
@@ -9,12 +9,18 @@ module ActiveInteraction
       raise EmptyPipeline if @steps.empty?
       (function, interaction), *steps = @steps
       outcome = interaction.run(function.call(options))
-      steps.reduce(outcome) do |o, (f, i)|
-        o.valid? ? i.run(f.call(o.result)) : o
-      end
+      steps.reduce(outcome) { |o, (f, i)| bind(o, f, i) }
     end
 
     private
+
+    def bind(outcome, function, interaction)
+      if outcome.valid?
+        interaction.run(function.call(outcome.result))
+      else
+        outcome
+      end
+    end
 
     def lambdafy(thing)
       case thing
