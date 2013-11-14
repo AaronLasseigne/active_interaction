22f2af50762f6dca58e0e7c8b78f3786750773d4
Taylor Fausak <taylor@fausak.me>
2013-11-14 08:55:18 -0600

    Clean up internal variable names
    Everything we use is prefied with "_interaction_". We should prevent
    people from using variables like that.

--- lib/active_interaction/base.rb
+++ lib/active_interaction/base.rb
@@ -64,13 +64,13 @@ module ActiveInteraction
     end
 
     validate do
-      return unless @_interaction_errors
+      return unless @_interaction_runtime_errors
 
-      @_interaction_errors.symbolic.each do |attribute, symbols|
+      @_interaction_runtime_errors.symbolic.each do |attribute, symbols|
         symbols.each { |symbol| errors.add_sym(attribute, symbol) }
       end
 
-      @_interaction_errors.messages.each do |attribute, messages|
+      @_interaction_runtime_errors.messages.each do |attribute, messages|
         messages.each { |message| errors.add(attribute, message) }
       end
     end
@@ -78,7 +78,8 @@ module ActiveInteraction
     # Returns the inputs provided to {.run} or {.run!} after being cast based
     #   on the filters in the class.
     #
-    # @return [Hash] All inputs passed to {.run} or {.run!}.
+    # @return [Hash{Symbol => Object}] All inputs passed to {.run} or {.run!}.
+    #
     # @since 0.6.0
     def inputs
       self.class.filters.reduce({}) do |h, filter|
@@ -87,6 +88,8 @@ module ActiveInteraction
       end
     end
 
+    # @param options [Hash{Symbol => Object}]
+    #
     # @private
     def initialize(options = {})
       options = options.symbolize_keys
@@ -124,12 +127,12 @@ module ActiveInteraction
 
     # @private
     def errors
-      @errors ||= Errors.new(self)
+      @_interaction_errors ||= Errors.new(self)
     end
 
     # @private
     def valid?(*args)
-      super || instance_variable_set(:@_interaction_result, nil)
+      super || @_interaction_result = nil
     end
 
     # @private
@@ -142,11 +145,10 @@ module ActiveInteraction
         yield
       end
     end
-    private_class_method :transaction
 
-    # @private
+    # @return [Filters]
     def self.filters
-      @filters ||= Filters.new
+      @_interaction_filters ||= Filters.new
     end
 
     # @!macro [new] run_attributes
@@ -158,16 +160,16 @@ module ActiveInteraction
     #
     # @return [ActiveInteraction::Base] An instance of the class `run` is
     #   called on.
-    def self.run(options = {})
-      new(options).tap do |interaction|
+    def self.run(*args)
+      new(*args).tap do |interaction|
         if interaction.valid?
           result = transaction { interaction.execute }
 
           if interaction.errors.empty?
             interaction.instance_variable_set(:@_interaction_result, result)
           else
-            interaction.instance_variable_set(:@_interaction_errors,
-              interaction.errors.dup)
+            interaction.instance_variable_set(
+              :@_interaction_runtime_errors, interaction.errors.dup)
           end
         end
       end
@@ -181,8 +183,8 @@ module ActiveInteraction
     # @raise [InteractionInvalidError] if there are any errors on the model.
     #
     # @return The return value of {#execute}.
-    def self.run!(options = {})
-      outcome = run(options)
+    def self.run!(*args)
+      outcome = run(*args)
       if outcome.invalid?
         raise InteractionInvalidError, outcome.errors.full_messages.join(', ')
       end
