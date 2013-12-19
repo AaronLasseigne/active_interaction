9af4a62056cf5f9a298fe7654b3cdd180482b20a
Taylor Fausak <taylor@fausak.me>
2013-12-19 11:11:42 -0600

    Add specs for filters that don't match the name

--- spec/active_interaction/filter_spec.rb
+++ spec/active_interaction/filter_spec.rb
@@ -3,6 +3,7 @@
 require 'spec_helper'
 
 class ActiveInteraction::TestFilter < ActiveInteraction::Filter; end
+class TestFilter < ActiveInteraction::Filter; end
 
 describe ActiveInteraction::Filter, :filter do
   include_context 'filters'
@@ -19,5 +20,24 @@ describe ActiveInteraction::Filter, :filter do
     it_behaves_like 'a filter'
 
     let(:described_class) { ActiveInteraction::TestFilter }
+  end
+
+  context TestFilter do
+    let(:described_class) { TestFilter }
+
+    describe '.factory' do
+      it 'returns a Filter' do
+        expect(described_class.factory(described_class.name.to_sym))
+          .to eq described_class
+      end
+    end
+
+    describe '.slug' do
+      it 'raises an error' do
+        expect do
+          described_class.slug
+        end.to raise_error ActiveInteraction::InvalidClassError
+      end
+    end
   end
 end
