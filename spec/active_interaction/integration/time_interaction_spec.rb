762c183df019228fec266393b2475913f0fdaf9c
Taylor Fausak <taylor@fausak.me>
2013-12-20 10:18:21 -0600

    Re #90; add test for ActiveSupport bug
    Here it is failing before 0e34d4b:
    
      1) TimeInteraction with a time zone with a string returns the correct value
         Failure/Error: expect(result[:a]).to eq TimeZone.parse(a)
         NoMethodError:
           undefined method `parse' for TimeWithZone:Class

--- spec/active_interaction/integration/time_interaction_spec.rb
+++ spec/active_interaction/integration/time_interaction_spec.rb
@@ -4,21 +4,23 @@ require 'spec_helper'
 
 TimeZone = Class.new do
   def self.at(*args)
-    TimeWithZone.at(*args)
+    TimeWithZone.new(Time.at(*args))
+  end
+
+  def self.parse(*args)
+    TimeWithZone.new(Time.parse(*args))
   end
 end
 
 TimeWithZone = Class.new do
-  def self.at(*args)
-    new(Time.at(*args))
-  end
+  attr_reader :time
 
   def initialize(time)
     @time = time
   end
 
   def ==(other)
-    @time == other
+    time == other.time
   end
 end
 
@@ -31,15 +33,28 @@ describe TimeInteraction do
   it_behaves_like 'an interaction', :time, -> { Time.now }
 
   context 'with a time zone' do
-    let(:a) { rand(1 << 16) }
+    let(:a) { nil }
 
     before do
-      allow(Time).to receive(:zone).and_return(TimeZone)
       inputs.merge!(a: a)
+
+      allow(Time).to receive(:zone).and_return(TimeZone)
     end
 
-    it 'returns the correct value' do
-      expect(result[:a]).to eq Time.at(a)
+    context 'with an integer' do
+      let(:a) { rand(1 << 16) }
+
+      it 'returns the correct value' do
+        expect(result[:a]).to eq TimeZone.at(a)
+      end
+    end
+
+    context 'with a string' do
+      let(:a) { '2011-12-13T14:15:16Z' }
+
+      it 'returns the correct value' do
+        expect(result[:a]).to eq TimeZone.parse(a)
+      end
     end
   end
 end
