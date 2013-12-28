edf98c360c16cc514dc3e157368fd48b055a2abe
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-12-27 19:29:30 -0600

    removing gratuitous sort

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Coveralls.wear!
 
 require 'active_interaction'
 
-Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
