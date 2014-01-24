4781d332a29789e73afe7e517b71d916226ef004
Taylor Fausak <taylor@fausak.me>
2014-01-24 13:46:16 -0600

    Remove unnecessarily specific namespace

--- lib/active_interaction/version.rb
+++ lib/active_interaction/version.rb
@@ -5,5 +5,5 @@ module ActiveInteraction
   # The version number.
   #
   # @return [Gem::Version]
-  ActiveInteraction::VERSION = Gem::Version.new('1.0.0')
+  VERSION = Gem::Version.new('1.0.0')
 end
