8bf255b087ebbaab8db96cc6c4691a9607511853
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-04-18 15:42:56 -0500

    add docs for FilterColumn

--- lib/active_interaction/filter_column.rb
+++ lib/active_interaction/filter_column.rb
@@ -1,11 +1,30 @@
 # coding: utf-8
 
 module ActiveInteraction
+  # A minimal implementation of an `ActiveRecord::ConnectionAdapters::Column`.
+  #
   # @since 1.2.0
   class FilterColumn
     attr_reader :limit, :type
 
     class << self
+      # rubocop:disable LineLength
+
+      # Find or create the `FilterColumn` for a specific type.
+      #
+      # @example
+      #   FilterColumn.intern(:string)
+      #   # => #<ActiveInteraction::FilterColumn:0x007feeaa649c18 @type=:string>
+      #
+      #   FilterColumn.intern(:string)
+      #   # => #<ActiveInteraction::FilterColumn:0x007feeaa649c18 @type=:string>
+      #
+      #   FilterColumn.intern(:boolean)
+      #   # => #<ActiveInteraction::FilterColumn:0x007feeab8a0498 @type=:boolean>
+      #
+      # @param type [Symbol] A database column type.
+      #
+      # @return [FilterColumn]
       def intern(type)
         @columns ||= {}
 
@@ -15,18 +34,28 @@ module ActiveInteraction
           @columns[type] = new(type)
         end
       end
+      # rubocop:enable LineLength
 
       private :new
     end
 
+    # @param type [type] The database column type.
+    #
+    # @private
     def initialize(type)
       @type = type
     end
 
+    # Returns `true` if the column is either of type :integer or :float.
+    #
+    # @return [Boolean]
     def number?
       [:integer, :float].include?(type)
     end
 
+    # Returns `true` if the column is of type :string.
+    #
+    # @return [Boolean]
     def text?
       type == :string
     end
