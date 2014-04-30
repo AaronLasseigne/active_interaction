a3be06e1ded576affe07f69cc6f4eb08fe83fc77
Taylor Fausak <taylor@fausak.me>
2014-04-30 08:04:04 -0500

    Add documentation

--- lib/active_interaction/grouped_input.rb
+++ lib/active_interaction/grouped_input.rb
@@ -3,6 +3,9 @@
 require 'ostruct'
 
 module ActiveInteraction
+  # Holds a group of inputs together for passing from {Base} to {Filter}s.
+  #
+  # @private
   class GroupedInput < OpenStruct
     def [](name)
       return super if self.class.superclass.method_defined?(:[])
