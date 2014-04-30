4a3a4765a6cd073acc1cc3703ea3b234c94ef12b
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-04-30 15:07:58 -0500

    just describe why they are needed

--- lib/active_interaction/backports.rb
+++ lib/active_interaction/backports.rb
@@ -3,12 +3,12 @@
 # rubocop:disable Documentation
 module ActiveInteraction
   class GroupedInput
-    # Remove when Ruby > 1.9.3.
+    # Required for Ruby <= 1.9.3.
     def [](name)
       send(name)
     end unless method_defined?(:[])
 
-    # Remove when Ruby > 1.9.3.
+    # Required for Ruby <= 1.9.3.
     def []=(name, value)
       send("#{name}=", value)
     end unless method_defined?(:[]=)
@@ -16,7 +16,7 @@ module ActiveInteraction
 end
 
 class Hash
-  # Remove when Rails > 4.0.2.
+  # Required for Rails <= 4.0.2.
   def transform_keys
     result = {}
     each_key do |key|
