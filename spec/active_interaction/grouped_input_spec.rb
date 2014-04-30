82692f253404c006e553e3d0e7f7d3549d8872cd
Taylor Fausak <taylor@fausak.me>
2014-04-30 08:18:01 -0500

    Add tests for #[] and #[]=

--- spec/active_interaction/grouped_input_spec.rb
+++ spec/active_interaction/grouped_input_spec.rb
@@ -8,4 +8,12 @@ describe ActiveInteraction::GroupedInput do
   it 'subclasses OpenStruct' do
     expect(grouped_input).to be_an OpenStruct
   end
+
+  it 'responds to #[]' do
+    expect { grouped_input[:key] }.to_not raise_error
+  end
+
+  it 'responds to #[]=' do
+    expect { grouped_input[:key] = :value }.to_not raise_error
+  end
 end
