1de37f912ea0bd12348293e57b9e7ae9a8ed0a11
Taylor Fausak <taylor@fausak.me>
2014-04-29 20:26:39 -0500

    Define methods for backwards compatibility

--- lib/active_interaction/grouped_input.rb
+++ lib/active_interaction/grouped_input.rb
@@ -4,5 +4,16 @@ require 'ostruct'
 
 module ActiveInteraction
   class GroupedInput < OpenStruct
+    def [](name)
+      return super if self.class.superclass.method_defined?(:[])
+
+      send(name)
+    end
+
+    def []=(name, value)
+      return super if self.class.superclass.method_defined?(:[]=)
+
+      send("#{name}=", value)
+    end
   end
 end
