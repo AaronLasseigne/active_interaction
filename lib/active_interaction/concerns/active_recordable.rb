6eaa6531eeb7a46c35c1a3b5c73630ce74ee1ccc
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2015-01-29 15:29:29 -0600

    add has_attribute? method for simple_form gem

--- lib/active_interaction/concerns/active_recordable.rb
+++ lib/active_interaction/concerns/active_recordable.rb
@@ -29,5 +29,32 @@ module ActiveInteraction
       filter = self.class.filters[name]
       FilterColumn.intern(filter.database_column_type) if filter
     end
+
+    # Returns true if a filter of that name exists.
+    #
+    # @param name [String, Symbol] The name of a filter.
+    #
+    # @example
+    #   class Interaction < ActiveInteraction::Base
+    #     string :email, default: nil
+    #
+    #     def execute; end
+    #   end
+    #
+    #   Interaction.new.has_attribute?(:email)
+    #   # => true
+    #
+    #   Interaction.new.has_attribute?(:not_a_filter)
+    #   # => false
+    #
+    # @return [Boolean]
+    #
+    # @since 1.5.0
+    #
+    # rubocop:disable PredicateName
+    def has_attribute?(name)
+      self.class.filters.key?(name.to_sym)
+    end
+    # rubocop:enable PredicateName
   end
 end
--- spec/active_interaction/concerns/active_recordable_spec.rb
+++ spec/active_interaction/concerns/active_recordable_spec.rb
@@ -9,8 +9,9 @@ end
 describe ActiveInteraction::ActiveRecordable do
   include_context 'interactions'
 
+  let(:described_class) { InteractionWithFilter }
+
   describe '#column_for_attribute(name)' do
-    let(:described_class) { InteractionWithFilter }
     let(:column) { outcome.column_for_attribute(name) }
 
     context 'name is not an input name' do
@@ -31,6 +32,20 @@ describe ActiveInteraction::ActiveRecordable do
       it 'returns a FilterColumn of type boolean' do
         expect(column.type).to eql :float
       end
+    end
+  end
+
+  describe '#has_attribute?' do
+    it 'returns true if the filter exists' do
+      expect(outcome.has_attribute?(:thing)).to be_truthy
+    end
+
+    it 'works with strings' do
+      expect(outcome.has_attribute?('thing')).to be_truthy
+    end
+
+    it 'returns false if the filter does not exist' do
+      expect(outcome.has_attribute?(:not_a_filter)).to be_falsey
     end
   end
 end
