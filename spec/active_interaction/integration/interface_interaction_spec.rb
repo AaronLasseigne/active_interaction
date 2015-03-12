e52dd8b1b6386e89665830a65d1e8a267155411c
Taylor Fausak <taylor@fausak.me>
2015-03-12 10:17:53 -0500

    Fix #263; allow nil for interfaces and models

--- spec/active_interaction/integration/interface_interaction_spec.rb
+++ spec/active_interaction/integration/interface_interaction_spec.rb
@@ -4,9 +4,18 @@ require 'spec_helper'
 require 'json'
 require 'yaml'
 
-describe 'InterfaceInteraction' do
+InterfaceInteraction = Class.new(TestInteraction) do
+  interface :anything
+end
+
+describe InterfaceInteraction do
+  include_context 'interactions'
   it_behaves_like 'an interaction',
     :interface,
     -> { [JSON, YAML].sample },
     methods: [:dump, :load]
+
+  it 'does not raise an error' do
+    expect { result }.to_not raise_error
+  end
 end
--- spec/active_interaction/integration/model_interaction_spec.rb
+++ spec/active_interaction/integration/model_interaction_spec.rb
@@ -2,6 +2,15 @@
 
 require 'spec_helper'
 
-describe 'ModelInteraction' do
+ModelInteraction = Class.new(TestInteraction) do
+  model :object
+end
+
+describe ModelInteraction do
+  include_context 'interactions'
   it_behaves_like 'an interaction', :model, -> { // }, class: Regexp
+
+  it 'does not raise an error' do
+    expect { result }.to_not raise_error
+  end
 end
