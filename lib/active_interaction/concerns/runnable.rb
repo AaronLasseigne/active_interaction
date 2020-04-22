21c1f197d9a4058270a559c0faa637ebef2c63f2
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2020-04-22 13:36:34 -0500

    fix rubocop errors

--- lib/active_interaction/concerns/runnable.rb
+++ lib/active_interaction/concerns/runnable.rb
@@ -77,9 +77,9 @@ module ActiveInteraction
         self.result =
           begin
             execute
-          rescue Interrupt => interrupt
-            errors.backtrace = interrupt.errors.backtrace || interrupt.backtrace
-            errors.merge!(interrupt.errors)
+          rescue Interrupt => e
+            errors.backtrace = e.errors.backtrace || e.backtrace
+            errors.merge!(e.errors)
           end
       end
     end
