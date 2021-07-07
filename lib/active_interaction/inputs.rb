524ca253cebed5b6e429a73a430f6dbc646c8a3e
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2021-07-06 20:58:40 -0500

    remove unused code

--- lib/active_interaction/inputs.rb
+++ lib/active_interaction/inputs.rb
@@ -73,48 +73,8 @@ module ActiveInteraction
       end
     end
 
-    def initialize
-      @groups = {}
-      @groups.default_proc = ->(hash, key) { hash[key] = [] }
-
-      super(@inputs = {})
-    end
-
-    # Associates the `value` with the `key`. Allows the `key`/`value` pair to
-    #   be associated with one or more groups.
-    #
-    # @example
-    #   inputs.store(:key, :value)
-    #   # => :value
-    #   inputs.store(:key, :value, %i[a b])
-    #   # => :value
-    #
-    # @param key [Object] The key to store the value under.
-    # @param value [Object] The value to store.
-    # @param groups [Array<Object>] The groups to store the pair under.
-    #
-    # @return [Object] value
-    # @private
-    def store(key, value, groups = [])
-      groups.each do |group|
-        @groups[group] << key
-      end
-
-      super(key, value)
-    end
-
-    # Returns inputs from the group name given.
-    #
-    # @example
-    #   inputs.group(:a)
-    #   # => {key: :value}
-    #
-    # @param name [Object] Name of the group to return.
-    #
-    # @return [Hash] Inputs from the group name given.
-    # @private
-    def group(name)
-      @inputs.select { |k, _| @groups[name].include?(k) }
+    def initialize(inputs = {})
+      super(inputs)
     end
   end
 end
