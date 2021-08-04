7ddda0d3d78d11e1dfe1e68f6027819ffe7fb1f6
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2021-08-03 21:56:04 -0500

    fix documentation

--- lib/active_interaction/base.rb
+++ lib/active_interaction/base.rb
@@ -186,7 +186,7 @@ module ActiveInteraction
     # Returns the inputs provided to {.run} or {.run!} after being cast based
     #   on the filters in the class.
     #
-    # @return [Hash{Symbol => Object}] All inputs passed to {.run} or {.run!}.
+    # @return [ActiveInteraction::Inputs] All expected inputs passed to {.run} or {.run!}.
     def inputs
       @_interaction_inputs
     end
